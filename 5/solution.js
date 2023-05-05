5a0ff73a6377a9a07bd0239c1ad5b28c1e4f4be1
Jing Hui P <enkr99@gmail.com>
2023-05-06 06:00:22 +0800

    :sparkles: [q5] Submitted solution, beat 43+,50+.

--- 5/solution.js
+++ 5/solution.js
@@ -3,51 +3,26 @@
  * @return {string}
  */
 const longestPalindrome = (s) => {
-  original_s = s;
   s = reformat(s);
-
-  let l = Array(s.length).fill(0), highest_length = 0, maxRight = 0, center = 0;
-
-  console.log(`s: ${s}, l: ${l}, h: ${highest_length}, center: ${center}, right: ${maxRight}, sl: ${s.length}`);
+  let l = Array(s.length).fill(0), highest_length = 0, center = 0;
 
   for (let i = 0; i < s.length; i++) {
-    // left = (i * 2) - i;
-    // console.log("___ : " + s[left] + ",  i: " + i + ", left: " + left)
-    console.log("----> i: " + i + ", s[i]: " + s[i])
-    // left
     for (let j = 0; j < i; j++) {
-      console.log("---> i: " + i + ", j: " + j)
       left = s[i - (j + 1)];
       right = s[i + (j + 1)];
-      console.log("left: (" + (i - (j + 1)) + ") - " + left)
-      console.log("right: (" + (i + (j + 1)) + ") - " + right)
 
-      if (left !== right) {
-        console.log("break bc the first neighbour already unmatched")
-        break;
-      }
-
-      console.log("ctn ...")
-      // console.log(l[i])
+      if (left !== right) break;
 
       l[i] = l[i] + 1;
+
       if (l[i] > highest_length) {
         highest_length = l[i];
         center = i;
       }
-
-      console.log(l[i])
     }
-
   }
 
-  console.log("finally list of match count: " + l)
-  console.log("highest_length: " + highest_length)
-  console.log("center: " + center)
-  s_part = s.substring((center - highest_length), (center + highest_length));
-  answer = s_part.replace(/#/g, "");
-  console.log(answer);
-  return answer;
+  return s.substring((center - highest_length), (center + highest_length)).replace(/#/g, "");
 };
 
 const reformat = (s) => {
@@ -60,6 +35,3 @@ const reformat = (s) => {
 
   return `${rs}#`;
 }
-
-longestPalindrome("NBNNBR")
-// longestPalindrome("qweeeewqwe")
