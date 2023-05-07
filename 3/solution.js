913a63f07fdce0717a7875b1650a2e29dcaa9d6d
Jing Hui P <enkr99@gmail.com>
2023-05-07 18:20:33 +0800

    :sparkles: [q3] New beat! 89.19%, 92.70%

--- 3/solution.js
+++ 3/solution.js
@@ -3,44 +3,22 @@
  * @return {number}
  */
 const lengthOfLongestSubstring = (s) => {
-  // Put into a map {index: 0, char: "a", nbAppeared: 1}
-  let tmpMap = new Map(),
-    charList = s.split(''),
-    tmpList = [], // Deciding result
-    result = [];
-
-  var prevTmpList = [];
+  let charList = s.split(''),
+    tmpList = [],
+    result = [],
+    prevTmpList = [];
 
   for (let i = 0; i < charList.length; i++) {
-    // Might need extra handle if there is upper/lower case.
-    let currChar = charList[i],
-      existCharMap = tmpMap.get(currChar);
+    let currChar = charList[i];
 
-    if (existCharMap) {
-      if (tmpList.includes(currChar)) {
-        if (tmpList.length > prevTmpList.length) prevTmpList = [...tmpList];
+    if (tmpList.includes(currChar) && tmpList.length > prevTmpList.length) prevTmpList = [...tmpList];
 
-        let nbRemove = tmpList.indexOf(currChar);
-        for (let j = 0; j <= nbRemove; j++) {
-          tmpList.shift();
-        }
-        tmpList.push(currChar);
-
-      } else {
-        for (let j = 0; j <= tmpList.indexOf(currChar); j++) {
-          tmpList.shift();
-        }
-        tmpList.push(currChar);
-      }
-
-      // Update values
-      existCharMap.nbAppeared += 1;
-      tmpMap.set(currChar, existCharMap);
-    } else {
-      // Set it into the map if doesnt exists
-      tmpMap.set(currChar, { index: i, nbAppeared: 1 });
-      tmpList.push(currChar)
+    let nbRemove = tmpList.indexOf(currChar);
+    for (let j = 0; j <= nbRemove; j++) {
+      tmpList.shift();
     }
+
+    tmpList.push(currChar);
 
     result = prevTmpList.length > tmpList.length ? prevTmpList : tmpList;
   }
