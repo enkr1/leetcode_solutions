32e57f5082417c2b968d121773bf737218c67ad0
Jing Hui P <enkr99@gmail.com>
2023-05-11 02:37:54 +0800

    :sparkles: [q4] Second attempt, PASSED - 68% beats

--- 4/solution.js
+++ 4/solution.js
@@ -4,28 +4,6 @@
  * @return {number}
  */
 const findMedianSortedArrays = (nums1, nums2) => {
-  let sortedList = reconstructList(nums1.concat(nums2), []);
-  return Number.isInteger(sortedList.length / 2) ?
-    (sortedList[sortedList.length / 2] + sortedList[(sortedList.length / 2) - 1]) / 2
-    : sortedList[(sortedList.length / 2) - .5];
-
-}
-
-const reconstructList = (numsList, sortedList) => {
-  if (0 === numsList.length) return sortedList;
-
-  let first = numsList[0],
-    biggerThanList = [];
-
-  numsList.shift();
-
-  for (let i = 0; i < numsList.length; i++) {
-    if (numsList[i] > first) {
-      biggerThanList = [...biggerThanList, numsList[i]];
-    } else {
-      sortedList = [...sortedList, numsList[i]];
-    }
-  }
-
-  return reconstructList(biggerThanList, [...sortedList, first]);
-}
+  let sortedList = nums1.concat(nums2).sort((a, b) => { return a - b });
+  return Number.isInteger(sortedList.length / 2) ? (sortedList[sortedList.length / 2] + sortedList[(sortedList.length / 2) - 1]) / 2 : sortedList[(sortedList.length / 2) - .5];
+};
