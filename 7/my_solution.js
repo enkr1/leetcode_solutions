cde58a8bb8d6520a312934c0f9354978882af943
Jing Hui P <enkr99@gmail.com>
2023-05-15 02:07:30 +0800

    :sparkles: [q7] Too easy - Runtime 68 ms Beats 73.85% Memory 44.2 MB Beats 30.76%

--- 7/my_solution.js
+++ 7/my_solution.js
@@ -4,26 +4,29 @@
  */
 const reverse = (x) => {
   let charList = x.toString().split(''), result = null;
-  // charList.forEach((char, i) => {
-  //   console.log("char")
-  //   console.log(char)
-  //   console.log(i)
-  //   console.log("parseInt(char[i]) * i")
-  //   console.log(parseInt(char[i]) * i)
-  //   result += parseInt(char[i]) * i;
-  //   i *= 10;
-  // })
+  console.log(x);
+  // console.log(x >>> 0);
+  console.log((2 ** 31) - 1);
 
-  result = reverseCharList(charList, 0, 1, null);
+  result = reverseCharList(charList, 0, 1, null, false);
 
   console.log("result")
   console.log(result)
 };
 
-const reverseCharList = (charList, i, factor, result) => {
-  if (undefined === charList[i]) return result;
+const reverseCharList = (charList, i, factor, result, isNeg) => {
+  if (undefined === charList[i]) {
+    if ((2 ** 31) <= result) return 0;
+
+    return isNeg ? result * -1 : result;
+  }
   console.log("charList[i]")
   console.log(charList[i])
+  if ("-" === charList[i]) {
+    isNeg = true;
+    i++;
+    return reverseCharList(charList, i, factor, result, isNeg);
+  }
 
   result += (parseInt(charList[i]) * factor);
   factor *= 10;
@@ -33,9 +36,11 @@ const reverseCharList = (charList, i, factor, result) => {
   console.log(factor)
 
   i++;
-  return reverseCharList(charList, i, factor, result);
+  return reverseCharList(charList, i, factor, result, isNeg);
 }
 
 
 // reverse(321)
-reverse(120)
+// reverse(120)
+// reverse(-123)
+reverse(1534236469)
--- 7/solution.js
+++ 7/solution.js
@@ -3,16 +3,24 @@
  * @return {number}
  */
 const reverse = (x) => {
-  return reverseCharList(x.toString().split(''), 0, 1, null);
+  return reverseCharList(x.toString().split(''), 0, 1, null, false);
 };
 
-const reverseCharList = (charList, i, factor, result) => {
-  if (undefined === charList[i]) return result;
+const reverseCharList = (charList, i, factor, result, isNeg) => {
+  if (undefined === charList[i]) {
+    if ((2 ** 31) <= result) return 0;
+    return isNeg ? result * -1 : result;
+  }
+
+  if ("-" === charList[i]) {
+    isNeg = true;
+    i++;
+    return reverseCharList(charList, i, factor, result, isNeg);
+  }
 
   result += (parseInt(charList[i]) * factor);
   factor *= 10;
   i++;
 
-  return reverseCharList(charList, i, factor, result);
+  return reverseCharList(charList, i, factor, result, isNeg);
 }
-
