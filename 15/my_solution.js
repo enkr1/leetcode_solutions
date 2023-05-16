65e6547e6fdf2ea8d8ce35210d040708cca2b6e5
Jing Hui P <enkr99@gmail.com>
2023-05-16 08:40:58 +0800

    :sparkles: [q15] Runtime 161 ms Beats 99.8% Memory 59.3 MB Beats 24.86%

--- 15/my_solution.js
+++ 15/my_solution.js
@@ -8,109 +8,41 @@ const threeSum = (nums) => {
   console.log(nums)
 
   for (let i = 0; i < nums.length; i++) {
-    if (undefined !== nums[i - 1] && nums[i - 1] === nums[i]) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
       console.log("-------------------> !!!! same as prev")
       console.log(nums[i - 1])
       console.log(nums[i])
       continue;
     }
 
-    let tmpList = [...nums];
-    tmpList.splice(i, 1);
+    let l = i + 1, r = nums.length - 1;
+    while (r > l) {
+      let n = (nums[i] + nums[l] + nums[r]);
+      console.log("n")
+      console.log(n)
 
-    let b = (0 === nums[i]) ? 0 : -nums[i];
-    let c = nums[i] + b;
-    console.log(`i: ${i}, nums[i]: ${nums[i]}, b: ${b}, c: ${c}`);
-    console.log("tmpList")
-    console.log(tmpList)
-
-    // // Build a hash map except the current item in order to find the other 2 group mates.
-    // for (let j = 0; j < tmpList.length; j++) {
-    //   tmpMap[tmpList[j]] = tmpList[j]
-    // }
-    // console.log(tmpMap)
-
-    result = findGroup(Math.min(...nums), Math.max(...nums), nums[i], b, c, tmpList, result);
+      if (0 === n) {
+        console.log(`Pushing ${i}`)
+        result.push([nums[i], nums[l], nums[r]]);
+        // r--;
+        l++;
+        while (nums[l] == nums[l - 1] && r > l) {
+          l++;
+        }
+      } else if (n > 0) {
+        r--;
+      } else { // < 0
+        l++;
+      }
+    }
   }
 
   console.log("result")
   console.log(result)
-  // console.log(removeDuplicates(result))
-  // console.log([...new Set(result.map(JSON.stringify))].map(JSON.parse));
 
-  return removeDuplicates(result);
+  return (result);
 };
 
-function compareArrays(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  arr1.sort();
-  arr2.sort();
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-function removeDuplicates(list) {
-  let uniqueSet = new Set();
-  let uniqueList = [];
-
-  for (let i = 0; i < list.length; i++) {
-    let isUnique = true;
-    let array = list[i];
-
-    for (let j = 0; j < uniqueList.length; j++) {
-      if (compareArrays(array, uniqueList[j])) {
-        isUnique = false;
-        break;
-      }
-    }
-
-    if (isUnique) {
-      uniqueSet.add(JSON.stringify(array));
-      uniqueList.push(array);
-    }
-  }
-
-  return uniqueList;
-}
-
-const findGroup = (min, max, a, b, c, list, result) => {
-  if (b > max || c < min) return result;
-
-  console.log(`-> min: ${min}, max: ${max}, a: ${a}, b: ${b}, c: ${c}, result: ${result}`);
-  // let tmpMap = { ...map }, tmp = [a];
-  let tmpList = [...list], tmp = [a];
-
-  if (tmpList.includes(b)) {
-    console.log("--> tmpList[b] Exist!")
-    console.log(b)
-    tmp = [...tmp, b];
-
-    tmpList.splice(tmpList.indexOf(b), 1)
-
-    if (tmpList.includes(c)) {
-      console.log("---> tmpList[c] Exist!")
-      console.log(c)
-
-      result.push([...tmp, c])
-      tmpList.splice(tmpList.indexOf(c), 1)
-
-      console.log("----> result");
-      console.log(result);
-    }
-  }
-
-  return findGroup(min, max, a, b + 1, c - 1, list, result);
-
-}
-
 // threeSum([0, 0, 0])
-threeSum([-1, 0, 1, 2, -1, -4])
+// threeSum([-1, 0, 1, 2, -1, -4])
+threeSum([-2, 0, 0, 2, 2])
--- 15/solution.js
+++ 15/solution.js
@@ -4,76 +4,30 @@
  */
 const threeSum = (nums) => {
   let result = [];
+  nums.sort((a, b) => a - b);
 
   for (let i = 0; i < nums.length; i++) {
-    let tmpList = [...nums];
-    tmpList.splice(i, 1);
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
 
-    let b = (0 === nums[i]) ? 0 : -nums[i];
-    let c = nums[i] + b;
+    let l = i + 1, r = nums.length - 1;
 
-    result = findGroup(Math.min(...nums), Math.max(...nums), nums[i], b, c, tmpList, result);
-  }
+    while (r > l) {
+      let n = (nums[i] + nums[l] + nums[r]);
 
-  return removeDuplicates(result);
-};
+      if (0 === n) {
+        result.push([nums[i], nums[l], nums[r]]);
 
-function compareArrays(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  arr1.sort();
-  arr2.sort();
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-function removeDuplicates(list) {
-  let uniqueSet = new Set();
-  let uniqueList = [];
-
-  for (let i = 0; i < list.length; i++) {
-    let isUnique = true;
-    let array = list[i];
-
-    for (let j = 0; j < uniqueList.length; j++) {
-      if (compareArrays(array, uniqueList[j])) {
-        isUnique = false;
-        break;
+        l++;
+        while (nums[l] == nums[l - 1] && r > l) {
+          l++;
+        }
+      } else if (n > 0) {
+        r--;
+      } else {
+        l++;
       }
     }
-
-    if (isUnique) {
-      uniqueSet.add(JSON.stringify(array));
-      uniqueList.push(array);
-    }
   }
 
-  return uniqueList;
-}
-
-const findGroup = (min, max, a, b, c, list, result) => {
-  if (b > max || c < min) return result;
-
-  let tmpList = [...list], tmp = [a];
-
-  if (tmpList.includes(b)) {
-    tmp = [...tmp, b];
-    tmpList.splice(tmpList.indexOf(b), 1)
-
-    if (tmpList.includes(c)) {
-      result.push([...tmp, c])
-      tmpList.splice(tmpList.indexOf(c), 1)
-    }
-  }
-
-  return findGroup(min, max, a, b + 1, c - 1, list, result);
-
-}
+  return result;
+};
