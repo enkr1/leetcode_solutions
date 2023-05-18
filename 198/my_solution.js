60ebeaf5aa6faf91f2d6fbc0b14038ad69bc0c70
Jing Hui P <enkr99@gmail.com>
2023-05-18 22:43:04 +0800

    :sparkles: [198] 2nd try after watching some YT
    Runtime 54 ms Beats 77.64% Memory 42.4 MB Beats 11.7%

--- 198/my_solution.js
+++ 198/my_solution.js
@@ -3,18 +3,37 @@
  * @return {number}
  */
 const rob = (nums) => {
-  let result = 0;
-  console.log("result")
-  console.log(result)
+  let tmpList = [];
 
-  for (let i = 0; i < nums.length; i += 2) {
-    console.log("nums[i]")
-    console.log(nums[i])
-    result += nums[i];
+  for (let i = 0; i < nums.length; i++) {
+    console.log(`------> i:${i}, nums[i]:${nums[i]}, nums[i-2]:${nums[i - 2]}`)
+    if (undefined === nums[i - 2]) {
+      console.log("-2 UNDEFINED")
+      tmpList.push(nums[i]);
+      continue;
+    }
+
+    console.log("- MAX")
+    console.log(tmpList)
+    console.log(tmpList.slice(0, i - 2 + 1)) // inclusive
+    console.log(Math.max(...tmpList.slice(0, i - 2 + 1))) // inclusive
+    // console.log(Math.max(tmpList[0], tmpList[i - 2]))
+    tmpList.push(nums[i] + (Math.max(...tmpList.slice(0, i - 2 + 1))))
+    console.log("tmpList")
+    console.log(tmpList)
   }
-  console.log("result")
-  console.log(result)
-  return result;
-};
 
-rob([1, 2, 3, 1]);
+  console.log("tmpList")
+  console.log(Math.max(...tmpList))
+  return;
+};
+// rob([1, 2])
+// rob([2, 1, 1, 2])
+rob([2, 3, 1, 2, 1, 1, 2])
+
+// [2,1,1,2]
+// Use Testcase
+// Output
+// 3
+// Expected
+// 4
--- 198/solution.js
+++ 198/solution.js
@@ -3,12 +3,16 @@
  * @return {number}
  */
 const rob = (nums) => {
-  let result = 0;
+  let tmpList = [];
 
-  for (let i = 0; i < nums.length; i += 2) {
-    result += nums[i];
+  for (let i = 0; i < nums.length; i++) {
+    if (undefined === nums[i - 2]) {
+      tmpList.push(nums[i]);
+      continue;
+    }
+
+    tmpList.push(nums[i] + (Math.max(...tmpList.slice(0, i - 2 + 1))))
   }
 
-  return result;
+  return Math.max(...tmpList);
 };
-
