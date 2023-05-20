eae6ce4e409eb219a4f5c147b4e038617ae9f975
Jing Hui P <enkr99@gmail.com>
2023-05-21 02:03:34 +0800

    :sparkles: [53] a2 - Runtime 80 ms Beats 66.33% Memory 50.3 MB Beats 59.37%

--- 53/my_solution.js
+++ 53/my_solution.js
@@ -3,35 +3,33 @@
  * @return {number}
  */
 const maxSubArray = (nums) => {
-  let tmpMax = 0, max = 0;
+  let tmpMax = nums[0], max = nums[0];
 
-  for (let i = 0; i < nums.length; i++) {
-    if (0 === i) {
-      tmpMax = nums[i];
-      max = tmpMax;
-      continue;
-    }
+  for (let i = 1; i < nums.length; i++) {
+    if (tmpMax < 0) tmpMax = 0;
+    tmpMax += nums[i];
+    if (tmpMax > max) max = tmpMax;
 
     // if c > m, restart m
-    if (0 >= tmpMax && nums[i] > tmpMax) {
-      console.log(nums[i] + " is nbiggger!")
-      console.log(tmpMax)
-      console.log(tmpMax + nums[i])
-
-      // when pre max and curr value sum is more than max, we restart max
-      if ((tmpMax + nums[i]) > max) { // c + m > max - restart the max
-        tmpMax = nums[i];
-        max = tmpMax;
-      } else { // but in the case where pre = -2 and current = - 1 and they are smaller than max, we reset the max if curr (tmpMax) is bigger than max, in this case -1 > -2, so replace.
-        console.log("WTF?")
-        tmpMax = nums[i];
-        if (tmpMax > max) max = tmpMax;
-
-      }
-    } else {
-      tmpMax += nums[i];
-      if (tmpMax > max) max = tmpMax;
-    }
+    //     if (0 >= tmpMax && nums[i] > tmpMax) {
+    //       console.log(nums[i] + " is nbiggger!")
+    //       console.log(tmpMax)
+    //       console.log(tmpMax + nums[i])
+    //
+    //       // when pre max and curr value sum is more than max, we restart max
+    //       if ((tmpMax + nums[i]) > max) { // c + m > max - restart the max
+    //         tmpMax = nums[i];
+    //         max = tmpMax;
+    //       } else { // but in the case where pre = -2 and current = - 1 and they are smaller than max, we reset the max if curr (tmpMax) is bigger than max, in this case -1 > -2, so replace.
+    //         console.log("WTF?")
+    //         tmpMax = nums[i];
+    //         if (tmpMax > max) max = tmpMax;
+    //
+    //       }
+    //     } else {
+    //       tmpMax += nums[i];
+    //       if (tmpMax > max) max = tmpMax;
+    //     }
   }
 
   console.log("max")
@@ -40,10 +38,10 @@ const maxSubArray = (nums) => {
   return max;
 };
 
-// maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
 // maxSubArray([5, 4, -1, 7, 8])
 // maxSubArray([2, 5, 9, 2, 6, 8, -10])
 // maxSubArray([-2, -1])
 // maxSubArray([1, -1])
 // maxSubArray([-11, -1])
-maxSubArray([3, -3, 2, -3]) // 3
+// maxSubArray([3, -3, 2, -3]) // 3
--- 53/solution.js
+++ 53/solution.js
@@ -3,28 +3,43 @@
  * @return {number}
  */
 const maxSubArray = (nums) => {
-  let tmpMax = 0, max = 0;
+  let tmpMax = nums[0], max = nums[0];
 
-  for (let i = 0; i < nums.length; i++) {
-    if (0 === i) {
-      tmpMax = nums[i];
-      max = tmpMax;
-      continue;
-    }
-
-    if (0 >= tmpMax && nums[i] > tmpMax) {
-      if ((tmpMax + nums[i]) > max) {
-        tmpMax = nums[i];
-        max = tmpMax;
-      } else {
-        tmpMax = nums[i];
-        if (tmpMax > max) max = tmpMax;
-      }
-    } else {
-      tmpMax += nums[i];
-      if (tmpMax > max) max = tmpMax;
-    }
+  for (let i = 1; i < nums.length; i++) {
+    if (tmpMax < 0) tmpMax = 0;
+    tmpMax += nums[i];
+    if (tmpMax > max) max = tmpMax;
   }
 
   return max;
 };
+
+/*
+
+2nd approach
+
+Time Complexity:
+The code uses a loop to iterate over the input array nums, starting from index 1. Inside the loop, there are some conditional statements and arithmetic operations that take constant time. Therefore, the time complexity of the code is determined by the loop itself.
+
+The loop iterates through the array once, from index 1 to the end. Hence, the time complexity is O(n-1), which simplifies to O(n), where n is the length of the input array nums.
+
+Space Complexity:
+The code uses a constant amount of additional space to store variables tmpMax and max. Regardless of the size of the input array, the space complexity remains constant. Therefore, the space complexity of the code is O(1).
+
+In summary, the time complexity of the code is O(n), and the space complexity is O(1).
+
+*/
+
+/*
+
+1st approach
+
+Time Complexity:
+The code uses a loop to iterate over the input array nums. Inside the loop, there are some conditional statements and arithmetic operations. Since the operations inside the loop take constant time, the time complexity of the code is determined by the loop itself. Therefore, the time complexity of the code is O(n), where n is the length of the input array nums.
+
+Space Complexity:
+The code uses a constant amount of additional space to store variables tmpMax and max. Regardless of the size of the input array, the space complexity remains constant. Therefore, the space complexity of the code is O(1).
+
+In summary, the time complexity of the code is O(n), and the space complexity is O(1).
+
+*/
