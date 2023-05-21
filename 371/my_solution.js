8d1a547119ae65ecf3dd50937b7f027b06f492d4
Jing Hui P <enkr99@gmail.com>
2023-05-21 22:56:51 +0800

    :ok: [testing] OK, now i understand bit manipulation

--- 371/my_solution.js
+++ 371/my_solution.js
@@ -4,11 +4,24 @@
  * @return {number}
  */
 const getSum = (a, b) => {
-  console.log(`a:${a}, b:${b}`)
-  console.log(Math.log(1))
-  console.log(Math.log(2))
-  a += b;
+  // TODO: OK TOTALLY DONT GET IT ...
+  while (b != 0) {
+    console.log(`--> a:${a}, b:${b}`)
+    console.log(`a & b :${a & b}`)
+
+    let tmp = (a & b) << 1;
+    console.log(`(a & b << 1) :${a & b << 1}`)
+    a = a ^ b;
+    console.log(`(a ^ b): ${a}`)
+    b = tmp;
+    console.log(`b:${b}`)
+  }
+  console.log("a")
+  console.log(a)
+
   return a;
 }
 
+
 getSum(1, 2)
+getSum(121, 239)
--- 371/solution.js
+++ 371/solution.js
@@ -4,23 +4,26 @@
  * @return {number}
  */
 const getSum = (a, b) => {
-  // TODO: OK TOTALLY DONT GET IT ...
   while (b != 0) {
     console.log(`--> a:${a}, b:${b}`)
     console.log(`a & b :${a & b}`)
 
-    let tmp = (a & b) << 1;
+    let tmp = (a & b) << 1; // bit shift
     console.log(`(a & b << 1) :${a & b << 1}`)
+
     a = a ^ b;
     console.log(`(a ^ b): ${a}`)
+
     b = tmp;
     console.log(`b:${b}`)
   }
+
   console.log("a")
   console.log(a)
 
   return a;
 }
 
+
 // getSum(1, 2)
-getSum(121, 239)
+// getSum(121, 239)
