8a50118723187248dea87e95b2b4bf8eb3e60ec2
Jing Hui P <enkr99@gmail.com>
2023-05-22 02:46:19 +0800

    :skull: [190] a1 - Failed

--- 190/my_solution.js
+++ 190/my_solution.js
@@ -3,5 +3,34 @@
  * @return {number} - a positive integer
  */
 const reverseBits = (n) => {
+  let str = "";
+  console.log(`check: ${(n)}`)
+  // n = (n) >> 1;
+  // n = (n) >> 1;
+  console.log(`check: ${n}`)
+  //
+  //   let reversed = 0;
+  //   const totalBits = 32;
+  //
+  //   for (let i = 0; i < totalBits; i++) {
+  //     reversed = (reversed << 1) | (n & 1);
+  //     n = n >> 1;
+  //   }
+  //   console.log(`check: ${reversed}`)
+
+  while (n != 0) {
+    console.log(`before: ${n}`)
+    str += (n & 1) ? "1" : "0";
+    n = n >> 1;
+    console.log(`after: ${n}`)
+    // n = 0;
+  }
+
+  console.log("str")
+  console.log(str)
+
   return;
 }
+
+reverseBits(00000010100101000001111010011100) // 43261596 -> 964176192
+// reverseBits(11111111111111111111111111111101)
