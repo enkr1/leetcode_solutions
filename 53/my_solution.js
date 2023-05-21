b002cc99ad5133087fcc775767ba3d665a62b358
Jing Hui P <enkr99@gmail.com>
2023-05-21 16:27:34 +0800

    :zap: [53] Trying ...

--- 53/my_solution.js
+++ 53/my_solution.js
@@ -9,27 +9,38 @@ const maxSubArray = (nums) => {
     if (tmpMax < 0) tmpMax = 0;
     tmpMax += nums[i];
     if (tmpMax > max) max = tmpMax;
+  }
 
+  console.log("max")
+  console.log(max)
+
+  return max;
+};
+
+const maxSubArrayOld = (nums) => {
+  let tmpMax = nums[0], max = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
     // if c > m, restart m
-    //     if (0 >= tmpMax && nums[i] > tmpMax) {
-    //       console.log(nums[i] + " is nbiggger!")
-    //       console.log(tmpMax)
-    //       console.log(tmpMax + nums[i])
-    //
-    //       // when pre max and curr value sum is more than max, we restart max
-    //       if ((tmpMax + nums[i]) > max) { // c + m > max - restart the max
-    //         tmpMax = nums[i];
-    //         max = tmpMax;
-    //       } else { // but in the case where pre = -2 and current = - 1 and they are smaller than max, we reset the max if curr (tmpMax) is bigger than max, in this case -1 > -2, so replace.
-    //         console.log("WTF?")
-    //         tmpMax = nums[i];
-    //         if (tmpMax > max) max = tmpMax;
-    //
-    //       }
-    //     } else {
-    //       tmpMax += nums[i];
-    //       if (tmpMax > max) max = tmpMax;
-    //     }
+    if (0 >= tmpMax && nums[i] > tmpMax) {
+      // console.log(nums[i] + " is nbiggger!")
+      // console.log(tmpMax)
+      // console.log(tmpMax + nums[i])
+
+      // when pre max and curr value sum is more than max, we restart max
+      if ((tmpMax + nums[i]) > max) { // c + m > max - restart the max
+        tmpMax = nums[i];
+        max = tmpMax;
+      } else { // but in the case where pre = -2 and current = - 1 and they are smaller than max, we reset the max if curr (tmpMax) is bigger than max, in this case -1 > -2, so replace.
+        console.log("WTF?")
+        tmpMax = nums[i];
+        if (tmpMax > max) max = tmpMax;
+
+      }
+    } else {
+      tmpMax += nums[i];
+      if (tmpMax > max) max = tmpMax;
+    }
   }
 
   console.log("max")
