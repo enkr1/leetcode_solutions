375a21383e4e8dff2f22fdcfe2a2bea35bd8e58c
Jing Hui P <enkr99@gmail.com>
2023-05-24 22:57:57 +0800

    :zap: [33] a2 - failed

--- 33/my_solution.js
+++ 33/my_solution.js
@@ -9,52 +9,31 @@ const search = (nums, target) => {
   // odd 5,7 => [2,0,1]
   while (l <= r) {
     let m = Math.ceil((l + r) / 2);
-    console.log(`l:${l}, r:${r}, m:${m}, target:${target}, nums[m]:${nums[m]}`)
-    if (target == nums[m]) {
-      console.log(`Returnig: ${m}`)
-      return m;
-    }
 
-    console.log("??")
+    if (target == nums[m]) return m;
+
     // chunk checking
     if (nums[l] <= nums[m]) {
-      // is the target within this chunk
 
-      if (nums[l] > target) {
-        l = m + 1;
-      } else {
-        // r = m;
-        // r = (m == r) ? m - 1 : m;
-        // l = m;
-        console.log("hit here")
-      }
-
-      //if (nums[l] <= target && target <= nums[m]) {
-      //	r = (m == r) ? m - 1 : r = m;
-      //} else {
-      //l = m + 1;
-      //}
-    } else {
       if (nums[l] <= target) {
-        r = m - 1;
+        r = m;
       } else {
         l = m + 1;
       }
-      // if the target >= m, l = m
-      // if the target <= m, r = m
-      // if (target >= m) l = m;
-      //if (target <= m) {
-      // 	r = (m == r) ? m - 1 : r = m;
-      //}
+    } else {
+      if (nums[m] <= target) {
+        l = m;
+      } else {
+        r = m - 1;
+      }
     }
+
     // if (nums[l] == nums[r]) return -1;
+
   }
 
-  console.log(`Returnig: -1`)
-
-  return -1;
+  // return -1;
 };
-
 
 
 
