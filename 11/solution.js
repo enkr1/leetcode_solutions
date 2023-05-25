0e8cee299122aa7509d840d99ff655350b468ab4
Jing Hui P <enkr99@gmail.com>
2023-05-25 22:03:26 +0800

    :memo: [11] t&s complexity

--- 11/solution.js
+++ 11/solution.js
@@ -3,6 +3,7 @@
  * @return {number}
 */
 const maxArea = (nums) => {
+  // O(n)
   let l = 0, r = nums.length - 1, maxArea = 0;
 
   while (l < r) {
@@ -18,3 +19,11 @@ const maxArea = (nums) => {
 
   return maxArea;
 }
+
+/*
+
+The space complexity of the given code is O(1) because it uses a constant amount of additional space regardless of the size of the input nums array. The code only declares a few variables (l, r, maxArea, area), but their space usage remains constant.
+
+The time complexity of the code is O(n), where n is the length of the input nums array. The code uses a two-pointer approach to find the maximum area between two lines in the array. It iterates over the array using the two pointers (l and r), and in each iteration, it computes the area and updates the maxArea variable. Since each element in the array is visited once, the time complexity is linear with respect to the size of the input array.
+
+*/
