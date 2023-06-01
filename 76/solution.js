baef5ac6115fdec8124da620b85f66765994bd78
Jing Hui P <enkr99@gmail.com>
2023-06-01 19:29:24 +0800

    :x: [76] a3

--- 76/solution.js
+++ 76/solution.js
@@ -4,31 +4,42 @@
  * @return {string}
  */
 const minWindow = (s, t) => {
-  let minStr = s, sChar = s.split(""), tChar = t.split(""), trackingMap = new Map(), maxHave = 0;
+  let l = -1, r = -1,
+    sChar = s.split(""), tChar = t.split(""),
+    trackingMap = new Map();
+
   tChar.forEach((item) => trackingMap.set(item, (trackingMap.get(item) || 0) + 1));
   if (JSON.stringify(sChar) === JSON.stringify(tChar)) return s;
 
+  // if the values of the map has a 0, skip saving the min - as we know that it’s not complete
+  // let sCount = sChar.length;
   for (let i = 0; i < sChar.length; i++) {
-    if (!tChar.includes(sChar[i])) continue;
+    if (!tChar.includes(sChar[i])) continue; // esc
 
-    let tmpStr = sChar[i],
-      visitMap = new Map([...trackingMap]),
+    // map in the loop, to keep the visit
+    visitMap = new Map([...trackingMap]), // reset visit count every loop
       have = 1;
-    visitMap.set(sChar[i], visitMap.get(sChar[i]) - 1);
 
-    if (have === tChar.length) minStr = tmpStr;
+    visitMap.set(sChar[i], visitMap.get(sChar[i]) - 1); // reduce the curr node
+
+    if (have === tChar.length) {
+      l = i;
+      r = i;
+      break;
+    }
 
     for (let k = i + 1; k < sChar.length; k++) {
       if (visitMap.has(sChar[k])) visitMap.set(sChar[k], visitMap.get(sChar[k]) - 1);
-      if (0 === visitMap.get(sChar[k])) have++;
 
-      tmpStr += sChar[k];
+      if (0 <= visitMap.get(sChar[k])) have++;
 
-      if (tChar.length === have && tmpStr.length < minStr.length) minStr = tmpStr;
+      if (tChar.length === have && ((r == -1 && l == -1) || ((k - i + 1) < (r - l + 1)))) {
+        l = i;
+        r = k;
+        break;
+      }
     }
-
-    maxHave = Math.max(have, maxHave);
   }
 
-  return (maxHave < tChar.length) ? "" : minStr || "";
+  return sChar.slice(l, r + 1).join('');
 }
