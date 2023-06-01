84a61a2f8e0e10d7c158d545d4b04f5fc2e9a363
Jing Hui P <enkr99@gmail.com>
2023-06-01 16:11:44 +0800

    :x: [76] 2 pointers?

--- 76/my_solution.js
+++ 76/my_solution.js
@@ -4,10 +4,8 @@
  * @return {string}
  */
 const minWindow = (s, t) => {
-  let minStr, sChar = s.split(""), tChar = t.split(""), trackingMap = new Map();
+  let minStr = s, sChar = s.split(""), tChar = t.split(""), trackingMap = new Map(), l = 0, r = 0;
   tChar.forEach((item) => trackingMap.set(item, (trackingMap.get(item) || 0) + 1));
-
-  if (JSON.stringify(sChar) === JSON.stringify(tChar)) return s;
 
   console.log("trackingMap")
   console.log(trackingMap)
@@ -16,61 +14,34 @@ const minWindow = (s, t) => {
   console.log('tChar')
   console.log(tChar)
 
-  // if the values of the map has a 0, skip saving the min - as we know that it’s not complete
-  for (let i = 0; i < sChar.length - 1; i++) {
-    if (!tChar.includes(sChar[i])) continue;
-    console.log(`-> i:${i} => sChar[i]:${sChar[i]}`)
+  let tmpStr = "", visitMap = new Map([...trackingMap])
+  while (r <= sChar.length) {
+    console.log(`-> l:${l}, r:${r}`)
+    console.log("tmpStr")
+    console.log(tmpStr)
 
-    // map in the loop, to keep the visit
-    let tmpStr = sChar[i];
-    let visitMap = new Map([...trackingMap]); // reset visit count every loop
-    visitMap.set(sChar[i], visitMap.get(sChar[i]) - 1); // reduce the curr node
 
-    for (let k = i + 1; k < sChar.length; k++) {
-      if (undefined === sChar[k]) continue;
 
-      if (visitMap.has(sChar[k])) visitMap.set(sChar[k], visitMap.get(sChar[k]) - 1);
+    if (!tChar.includes(sChar[l])) {
+      console.log("A")
+      l++;
+      r++;
+    } else {
+      console.log("B")
+      if (visitMap.has(sChar[r])) visitMap.set(sChar[r], visitMap.get(sChar[r]) - 1);
 
-      tmpStr += sChar[k];
-      // console.log(`--> k:${k} => sChar[k]:${sChar[k]}, tmpStr:${tmpStr}, minStr:${minStr}`)
-      // console.log("--> visitMap")
-      // console.log(visitMap)
+      tmpStr += sChar[r];
 
-      // save the tmpMin if all <1
-      // when it is the last one and it does not have every value < 1
-      if (k === sChar.length - 1) {
-        console.log("last iteration")
-        if ([...visitMap.values()].every((value) => 1 > value)) {
-          // we know all has been visited => min.
-          console.log(">>>>>>>>> ALL VISITED: " + tmpStr)
-
-          if (undefined === minStr) {
-            console.log("A")
-            minStr = tmpStr;
-          } else if (tmpStr.length > 0 && tmpStr.length < minStr.length) {
-            console.log("B")
-            minStr = tmpStr;
-            console.log("smaller than! -> " + minStr + "")
-          }
-          // if (undefined === minStr) {
-          //   console.log("A")
-          //   minStr = tmpStr;
-          // } else if (tmpStr.length > 0 && tmpStr.length < minStr.length) {
-          //   console.log("B")
-          //   minStr = tmpStr;
-          //   console.log("smaller than! -> " + minStr + "")
-          // }
-          // else {
-          //   console.log("C")
-          //   minStr = "";
-          // }
-        } else {
-          // When all has been checked (!visited), we reset to "".
-          console.log("Gone thru not NONE found")
-          // tmpStr = "";
-        }
-
+      // Save when map all values are 0
+      if ([...visitMap.values()].every((value) => 1 > value) && tmpStr.length < minStr.length) {
+        minStr = tmpStr;
+        tmpStr = "";
+        console.log("WQLNEKWEQLKEKQWEK new min")
+        console.log(minStr)
       }
+
+      r++;
+      // if ([...visitMap.values()].every((value) => 1 > value)) {
     }
   }
 
