b5edb76a4c9ea4400587b5e9d8656498179b2633
Jing Hui P <enkr99@gmail.com>
2023-06-02 04:25:31 +0800

    :sparkles: [125] Runtime 61 ms Beats 94.65% Memory 44.1 MB Beats 89.16%

--- 125/solution.js
+++ 125/solution.js
@@ -3,7 +3,7 @@
  * @return {boolean}
 */
 const isPalindrome = (s) => {
-  s = s.replace(/[^a-zA-Z0-9]+/g, '').trim().toLowerCase();
+  s = s.replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
   let l = 0, r = s.length - 1;
 
   while (l <= r) {
