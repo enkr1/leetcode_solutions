0da0a38f7d9b3a098fab03b74a1cd3bf7cf0ac13
Jing Hui P <enkr99@gmail.com>
2023-06-01 18:43:21 +0800

    :x: [76] a2 - maxHave

--- 76/my_solution.js
+++ 76/my_solution.js
@@ -4,82 +4,93 @@
  * @return {string}
  */
 const minWindow = (s, t) => {
-  let minStr, sChar = s.split(""), tChar = t.split(""), trackingMap = new Map();
+  let minStr = s, sChar = s.split(""), tChar = t.split(""), trackingMap = new Map(), maxHave = 0;
   tChar.forEach((item) => trackingMap.set(item, (trackingMap.get(item) || 0) + 1));
   if (JSON.stringify(sChar) === JSON.stringify(tChar)) return s;
-  console.log("trackingMap")
-  console.log(trackingMap)
-  console.log('sChar')
-  console.log(sChar)
-  console.log('tChar')
-  console.log(tChar)
+  // console.log("trackingMap")
+  // console.log(trackingMap)
+  // console.log('sChar')
+  // console.log(sChar)
+  // console.log('tChar')
+  // console.log(tChar)
 
-  const hasVisitedAll = (map, str) => {
-    if ([...map.values()].every((value) => 1 > value)) {
-      // we know all has been visited => min.
-      console.log(">>>>>>>>> ALL VISITED: " + str)
-      if (undefined === minStr) {
-        console.log("A str:" + str)
-        minStr = str;
-      } else if (str.length > 0 && str.length < minStr.length) {
-        console.log("B")
-        minStr = str;
-        console.log("smaller than! -> " + minStr + "")
-      }
-    } else {
-      // When all has been checked (!visited), we reset to "".
-      console.log("Gone thru not NONE found")
-    }
-  }
+  // const hasVisitedAll = (map, str) => {
+  //   if ([...map.values()].every((value) => 1 === value)) {
+  //     // we know all has been visited => min.
+  //     console.log(">>>>>>>>> ALL VISITED: " + str)
+  //     // if (undefined === minStr) {
+  //     //   console.log("A str:" + str)
+  //     //   minStr = str;
+  //     // } else if (str.length > 0 && str.length < minStr.length) {
+  //     if (str.length < minStr.length) {
+  //       console.log("B")
+  //       minStr = str;
+  //       console.log("smaller than! -> " + minStr + "")
+  //     }
+  //   } else {
+  //     // When all has been checked (!visited), we reset to "".
+  //     console.log("Gone thru not NONE found")
+  //   }
+  // }
 
   // if the values of the map has a 0, skip saving the min - as we know that it’s not complete
-  for (let i = 0; i < sChar.length ; i++) {
-    if (!tChar.includes(sChar[i])) continue;
+  for (let i = 0; i < sChar.length; i++) {
+    if (!tChar.includes(sChar[i])) {
+      console.log("ESC:" + sChar[i])
+      continue;
+    }
+
     console.log(`-> i:${i} => sChar[i]:${sChar[i]}`)
     // map in the loop, to keep the visit
-    let tmpStr = sChar[i];
-    let visitMap = new Map([...trackingMap]); // reset visit count every loop
+    let tmpStr = sChar[i],
+      visitMap = new Map([...trackingMap]), // reset visit count every loop
+      have = 1;
     visitMap.set(sChar[i], visitMap.get(sChar[i]) - 1); // reduce the curr node
 
-    hasVisitedAll(visitMap, tmpStr);
+    if (have === tChar.length) minStr = tmpStr;
 
     for (let k = i + 1; k < sChar.length; k++) {
-      // if (undefined === sChar[k]) continue;
+      console.log(`--> k:${k} => sChar[k]:${sChar[k]}, tmpStr:${tmpStr}, minStr:${minStr}`)
       if (visitMap.has(sChar[k])) visitMap.set(sChar[k], visitMap.get(sChar[k]) - 1);
+      console.log("curr val")
+      console.log(visitMap.get(sChar[k]))
+      if (0 === visitMap.get(sChar[k])) have++;
+
       tmpStr += sChar[k];
-      // console.log(`--> k:${k} => sChar[k]:${sChar[k]}, tmpStr:${tmpStr}, minStr:${minStr}`)
-      // console.log("--> visitMap")
-      // console.log(visitMap)
+
       // save the tmpMin if all <1
       // when it is the last one and it does not have every value < 1
-      hasVisitedAll(visitMap, tmpStr);
-      // if ([...visitMap.values()].every((value) => 1 > value)) {
-      //   // we know all has been visited => min.
-      //   console.log(">>>>>>>>> ALL VISITED: " + tmpStr)
-      //   if (undefined === minStr) {
-      //     console.log("A tmpStr:" + tmpStr)
-      //     minStr = tmpStr;
-      //   } else if (tmpStr.length > 0 && tmpStr.length < minStr.length) {
-      //     console.log("B")
-      //     minStr = tmpStr;
-      //     console.log("smaller than! -> " + minStr + "")
-      //   }
-      //   continue;
-      // } else {
-      //   // When all has been checked (!visited), we reset to "".
-      //   console.log("Gone thru not NONE found")
-      // }
+      // hasVisitedAll(visitMap, tmpStr);
+      if (tChar.length === have) {
+        if (tmpStr.length < minStr.length) {
+          console.log("FOUNDDD DDDDSKALKADSLKJADSJKLS")
+          minStr = tmpStr;
+        }
+      }
+
+      // console.log("--> visitMap")
+      // console.log(visitMap)
+      console.log("have")
+      console.log(have)
     }
+
+    maxHave = Math.max(have, maxHave);
   }
+
+
   console.log(">>>>>>>>>>>>>>>>>>>>>> minStr")
   console.log(minStr)
   console.log(minStr || "")
+  console.log((maxHave < tChar.length) ? "" : minStr)
+
   return minStr || "";
 }
 //         0123456789012
 // minWindow("ADOBECODEBANC", "ABC") // BANC
-minWindow("ADOBECODEBANCXYACB", "ABC") // BANC
+// minWindow("ADOBECODEBANCXYA", "ABC") // BANC
+// minWindow("ADOBECODEBANCXYACB", "ABC") // ACB
 // minWindow("a", "a") // a
 // minWindow("abc", "aa") // ""
 // minWindow("ab", "a") // "a"
 // minWindow("ab", "b") // "b"
+// minWindow("bbaa", "aba") // "b"
--- 76/solution.js
+++ 76/solution.js
@@ -4,37 +4,31 @@
  * @return {string}
  */
 const minWindow = (s, t) => {
-  let minStr, sChar = s.split(""), tChar = t.split(""), trackingMap = new Map();
+  let minStr = s, sChar = s.split(""), tChar = t.split(""), trackingMap = new Map(), maxHave = 0;
   tChar.forEach((item) => trackingMap.set(item, (trackingMap.get(item) || 0) + 1));
-
   if (JSON.stringify(sChar) === JSON.stringify(tChar)) return s;
 
-  const hasVisitedAll = (map, str) => {
-    if ([...map.values()].every((value) => 1 > value)) {
-      if (undefined === minStr) {
-        minStr = str;
-      } else if (str.length > 0 && str.length < minStr.length) {
-        minStr = str;
-      }
-    }
-  }
-
-  // if the values of the map has a 0, skip saving the min - as we know that it’s not complete
   for (let i = 0; i < sChar.length; i++) {
     if (!tChar.includes(sChar[i])) continue;
-    // map in the loop, to keep the visit
-    let tmpStr = sChar[i];
-    let visitMap = new Map([...trackingMap]); // reset visit count every loop
-    visitMap.set(sChar[i], visitMap.get(sChar[i]) - 1); // reduce the curr node
 
-    hasVisitedAll(visitMap, tmpStr);
+    let tmpStr = sChar[i],
+      visitMap = new Map([...trackingMap]),
+      have = 1;
+    visitMap.set(sChar[i], visitMap.get(sChar[i]) - 1);
+
+    if (have === tChar.length) minStr = tmpStr;
 
     for (let k = i + 1; k < sChar.length; k++) {
       if (visitMap.has(sChar[k])) visitMap.set(sChar[k], visitMap.get(sChar[k]) - 1);
+      if (0 === visitMap.get(sChar[k])) have++;
+
       tmpStr += sChar[k];
-      hasVisitedAll(visitMap, tmpStr);
+
+      if (tChar.length === have && tmpStr.length < minStr.length) minStr = tmpStr;
     }
+
+    maxHave = Math.max(have, maxHave);
   }
 
-  return minStr || "";
+  return (maxHave < tChar.length) ? "" : minStr || "";
 }
