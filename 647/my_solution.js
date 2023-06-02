0780662fbc998dbaef9bb809e400a6c2805cfc60
Jing Hui P <enkr99@gmail.com>
2023-06-02 23:40:14 +0800

    :zap: [647] Manacher's algorithm

--- 647/my_solution.js
+++ 647/my_solution.js
@@ -3,53 +3,89 @@
  * @return {number}
  */
 const countSubstrings = (s) => {
-  // init the default count = 0 // s.len
-  let l = 0, r = 0, count = 0;
+  let count = 0;
 
-  // while (l <= r && r < s.length) {
-  while (l <= r) {
+  // Manacher's algorithm
+  // preprocess to add #
+  processedS = `#`
+  for (let i = 0; i < s.length; i++) {
+    processedS += `${s[i]}#`;
+  }
+  console.log(processedS)
 
-    console.log(`l:${l}, r:${r} ?:${s.slice(l, r + 1)}`)
-    if (undefined === s[r]) {
-      console.log("not foudn alr")
+  for (let i = 0; i < processedS.length; i++) {
+    console.log(`-> i:${i}`)
+    let l = 1; r = 1, tmpCount = 0;
+    while (undefined !== processedS[i - l] && processedS[i - l] === processedS[i + r]) {
+      console.log(`--> l:${l}, r:${r}, ${processedS[i - l]} === ${processedS[i + r]}`)
+      tmpCount++;
       l++;
-      continue;
+      r++;
     }
 
-    if (isPalidrome(s.slice(l, r + 1))) {
-      console.log(`this is a pal: ${s.slice(l, r + 1)}`)
-      count++;
-    }
+    console.log("tmpCount")
+    console.log(tmpCount)
 
-    r++;
 
-    if (r === s.length) {
-      console.log("yo the ennd")
-      l++;
-      r = l;
-    }
+    count += Math.ceil(tmpCount / 2);
   }
 
-  console.log("count")
+
+  console.log(`>> ${s}:count`)
   console.log(count)
 
   return count;
 }
 
-// fn to findPalidrome while lr
-const isPalidrome = (str) => {
-  let x = 0, y = str.length - 1;
-  while (x <= y) {
-    if (str[x] !== str[y]) return false;
-    x++;
-    y--;
-  }
-
-  return true;
-}
-
 
 countSubstrings("abc") // 3
-countSubstrings("aaa") // 6
-countSubstrings("qwere") // 5
-countSubstrings("aba") // 4
+// countSubstrings("aaa") // 6
+// countSubstrings("qwere") // 6
+// countSubstrings("aba") // 4
+// countSubstrings("dnncbwoneinoplypwgbwktmvkoimcooyiwirgbxlcttgteqthcvyoueyftiwgwwxvxvg") // 6
+
+// const countSubstrings = (s) => {
+//   // init the default count = 0 // s.len
+//   let l = 0, r = 0, count = 0;
+//
+//   // while (l <= r && r < s.length) {
+//   while (l <= r) {
+//
+//     console.log(`l:${l}, r:${r} ?:${s.slice(l, r + 1)}`)
+//     if (undefined === s[r]) {
+//       console.log("not foudn alr")
+//       l++;
+//       continue;
+//     }
+//
+//     if (isPalidrome(s.slice(l, r + 1))) {
+//       console.log(`this is a pal: ${s.slice(l, r + 1)}`)
+//       count++;
+//     }
+//
+//     r++;
+//
+//     if (r === s.length) {
+//       console.log("yo the ennd")
+//       l++;
+//       r = l;
+//     }
+//   }
+//
+//   console.log("count")
+//   console.log(count)
+//
+//   return count;
+// }
+//
+// // fn to findPalidrome while lr
+// const isPalidrome = (str) => {
+//   let x = 0, y = str.length - 1;
+//   while (x <= y) {
+//     if (str[x] !== str[y]) return false;
+//     x++;
+//     y--;
+//   }
+//
+//   return true;
+// }
--- 647/solution.js
+++ 647/solution.js
@@ -2,36 +2,82 @@
  * @param {string} s
  * @return {number}
  */
-const countSubstrings = (s) => {
-  let l = 0, r = 0, count = 0;
+const countSubstrings = (s) => { // Manacher's algorithm
+  let modifiedString = '#';
+  for (let i = 0; i < s.length; i++) {
+    modifiedString += s[i] + '#';
+  }
 
-  while (l <= r) {
-    if (undefined === s[r]) {
-      l++;
-      continue;
-    }
-    if (isPalidrome(s.slice(l, r + 1))) {
-      count++;
+  const n = modifiedString.length;
+  const P = new Array(n).fill(0);
+  let center = 0, right = 0;
+  let count = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (i <= right) {
+      const mirror = 2 * center - i;
+      P[i] = Math.min(right - i, P[mirror]);
     }
 
-    r++;
+    let leftIndex = i - (1 + P[i]);
+    let rightIndex = i + (1 + P[i]);
 
-    if (r === s.length) {
-      l++;
-      r = l;
+    while (leftIndex >= 0 && rightIndex < n && modifiedString[leftIndex] === modifiedString[rightIndex]) {
+      P[i]++;
+      leftIndex--;
+      rightIndex++;
     }
+
+    if (i + P[i] > right) {
+      center = i;
+      right = i + P[i];
+    }
+
+    count += Math.ceil(P[i] / 2);
   }
 
   return count;
-}
+};
 
-const isPalidrome = (str) => {
-  let x = 0, y = str.length - 1;
-  while (x <= y) {
-    if (str[x] !== str[y]) return false;
-    x++;
-    y--;
-  }
 
-  return true;
-}
+// const countSubstrings = (s) => {
+//   let l = 0, r = 0, count = 0;
+//
+//   while (l <= r) {
+//     if (undefined === s[r]) {
+//       l++;
+//       continue;
+//     }
+//
+//     if (isPalidrome(s.slice(l, r + 1))) {
+//       count++;
+//     }
+//
+//     r++;
+//
+//     if (r === s.length) {
+//       l++;
+//       r = l;
+//     }
+//   }
+//
+//   console.log("count")
+//   console.log(count)
+//
+//   return count;
+// }
+//
+// const isPalidrome = (str) => {
+//   let x = 0, y = str.length - 1;
+//   while (x <= y) {
+//     if (str[x] !== str[y]) return false;
+//     x++;
+//     y--;
+//   }
+//
+//   return true;
+// }
+
+countSubstrings("aaa")
+countSubstrings("abcb")
+countSubstrings("qrwrw")
