350b39f725cba6e7f411844e050e75ec2e107938
Jing Hui P <enkr99@gmail.com>
2023-06-03 22:15:28 +0800

    :zap: [771]

--- 771/solution.js
+++ 771/solution.js
@@ -7,7 +7,8 @@ const numJewelsInStones = (jewels, stones) => {
   let count = 0, jMap = new Map();
 
   for (let i = 0; i < jewels.length; i++) {
-    jMap.set(jewels[i], (jMap.get(jewels[i]) || 0) + 1);
+    // jMap.set(jewels[i], (jMap.get(jewels[i]) || 0) + 1);
+    jMap.set(jewels[i], true);
   }
 
   for (let i = 0; i < stones.length; i++) {
