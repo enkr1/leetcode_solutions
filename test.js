8c068285456a9344885b0f7cd954e343c7642c13
Jing Hui P <enkr99@gmail.com>
2023-06-06 04:22:33 +0800

    :construction: [-]

--- test.js
+++ test.js
@@ -22,12 +22,26 @@
 // console.log(extractWords(str));
 
 
+//
+// let x = [1, 2, 3];
+// console.log(x); // Output: [1, 2, 3]
+//
+// let y = [...x];
+// // console.log(y.splice(2, 0, 4)); // Output: []
+// y.splice(0, 0, 4)
+//
+// console.log(y); // Output: [1, 2, 4, 3]
 
-let x = [1, 2, 3];
-console.log(x); // Output: [1, 2, 3]
+console.log(2 % 2 == 0);
+console.log(3 % 2 == 0);
+console.log(4 % 2 == 0);
 
-let y = [...x];
-// console.log(y.splice(2, 0, 4)); // Output: []
-y.splice(0, 0, 4)
-
-console.log(y); // Output: [1, 2, 4, 3]
+const fizzBuzz = (n) => {
+  let result = "";
+  if (n % 3 === 0) result = "Fizz"
+  if (n % 5 === 0) result += "Buzz"
+  return result;
+}
+let fb = fizzBuzz(15)
+console.log("fb")
+console.log(fb)
