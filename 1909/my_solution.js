3cbcd8691409c1bb21406d9623dcd93a94f9dded
Jing Hui P <enkr99@gmail.com>
2023-06-09 03:14:53 +0800

    :zap: [1909] a2

--- 1909/my_solution.js
+++ 1909/my_solution.js
@@ -4,17 +4,15 @@
  */
 const canBeIncreasing = (nums) => {
   for (let i = 1; i < nums.length; i++) {
-    if (nums[i] >= nums[i + 1]) {
-      console.log(`nums[i]:${nums[i]} > nums[i + 1]:${nums[i + 1]}`)
-      if (undefined === nums[i - 1]) {
-        continue;
-      } else {
-        console.log(`-> nums[i]:${nums[i]}`)
-        console.log(`-> nums[i + 1]:${nums[i + 1]} > nums[i - 1]:${nums[i - 1]}`)
-        console.log(`-> nums[i - 1]:${nums[i]} > nums[i + 1]:${nums[i + 2]}`)
-        console.log(`${i + 2} - ${nums.length}`)
-        return (nums[i + 1] > nums[i - 1]) || nums[i] < nums[i + 2] || i + 2 === nums.length;
-      }
+    console.log(`-> nums[i]:${nums[i]}`)
+    if (
+      (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) ||
+      (nums[i] < nums[i - 1] && nums[i] < nums[i + 1]) ||
+      (nums[i] === nums[i - 1] && nums[i] < nums[i + 1]) ||
+      (nums[i] > nums[i - 1] && nums[i] < nums[i + 1])
+    ) {
+    } else {
+      return false;
     }
   }
 
@@ -23,9 +21,9 @@ const canBeIncreasing = (nums) => {
 
 
 // let x = canBeIncreasing([1, 2, 10, 5, 7]); // true
-// let x = canBeIncreasing([1, 2, 5, 7]); // true
-// let x = canBeIncreasing([2,3,1,2]); // false
-let x = canBeIncreasing([1, 1, 1]); // false
+let x = canBeIncreasing([1, 2, 5, 7]); // true
+// let x = canBeIncreasing([2, 3, 1, 2]); // false
+// let x = canBeIncreasing([1, 1, 1]); // false
 // let x = canBeIncreasing([105, 924, 32, 968]); // true
 // let x = canBeIncreasing([512, 867, 904, 997, 403]); // true
 console.log(x)
