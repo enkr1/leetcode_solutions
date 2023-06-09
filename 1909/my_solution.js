91de16a573f2c1a6a744f67fbc83ce2d77a38e30
Jing Hui P <enkr99@gmail.com>
2023-06-09 23:18:33 +0800

    :white_check_mark: [1909] a5 - max edge + jumping comparison

--- 1909/my_solution.js
+++ 1909/my_solution.js
@@ -3,46 +3,44 @@
  * @return {boolean}
  */
 const canBeIncreasing = (nums) => {
-  let nbToRemove = 1;
+  let credit = 1, maxEdge = 0;
+  console.log(nums)
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] >= nums[i + 1]) {
+      if (0 === credit) return false;
+      let tmpMaxEdge = nums[i];
+      credit--;
+      console.log(`-> nums[i]:${nums[i]}, nums[i + 1]:${nums[i + 1]}, credit:${credit}, maxEgde:${maxEdge}`);
+      // new edge is lower means it is going down
+      if (tmpMaxEdge <= maxEdge) return false;
 
-  let curr = -Infinity, tmpList = [];
-  console.log(curr)
-  // tmpList.push(curr);
-
-  while (0 < nums.length) {
-    let next = nums.shift();
-    console.log(`-> curr:${curr}, next:${next}, nbToRemove:${nbToRemove}`);
-    // [2, 3, 1, 2]
-    // curr = 2, next = 3
-    // curr = 3, next = 1
-    // [2, 3, 1, 2]
-    // [2, 3, 1, 2]
-    if (curr >= next) {
-      console.log(`curr >= next`)
-      nbToRemove--;
-      if (nbToRemove < 0) return false;
-      // curr = next;
-      // tmpList.pop();
-    } else {
-      console.log(`tmping`)
-      // tmpList.push(next);
-      curr = next;
+      // if my within is not incline.
+      if (nums[i - 1] >= nums[i + 1]) {
+        if (undefined === nums[i + 2]) {
+          console.log("No more to iterate")
+          continue;
+          // return false; // No more to iterate
+        }
+        if (nums[i] >= nums[i + 2]) {
+          return false;
+        } else {
+          i++; // Skip the next one.
+        }
+      }
     }
-
   }
-
-  console.log(tmpList)
 
   return true;
 };
 
 
-let x = canBeIncreasing([10, 1, 2, 3]); // true
+// let x = canBeIncreasing([10, 1, 2, 3]); // true
 // let x = canBeIncreasing([2, 3, 1, 2]); // false
 // let x = canBeIncreasing([20, 10, 1, 2, 3]); // false
 // let x = canBeIncreasing([1, 2, 10, 5, 7]); // true
 // let x = canBeIncreasing([1, 2, 5, 7]); // true
-// let x = canBeIncreasing([1, 1, 1]); // false
+let x = canBeIncreasing([1, 1, 1]); // false
 // let x = canBeIncreasing([105, 924, 32, 968]); // true
 // let x = canBeIncreasing([512, 867, 904, 997, 403]); // true
+// let x = canBeIncreasing([541, 783, 433, 744]); // false
 console.log(x)
--- 1909/solution.js
+++ 1909/solution.js
@@ -3,11 +3,28 @@
  * @return {boolean}
  */
 const canBeIncreasing = (nums) => {
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] >= nums[i + 1]) {
-      if (undefined === nums[i - 1]) continue;
+  let credit = 1, maxEdge = 0;
 
-      return (nums[i + 1] > nums[i - 1]) || nums[i] < nums[i + 2] || i + 2 == nums.length;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] >= nums[i + 1]) {
+      if (0 === credit) return false;
+      let tmpMaxEdge = nums[i];
+      credit--;
+
+      if (tmpMaxEdge <= maxEdge) return false;
+
+      // if my within is not incline.
+      if (nums[i - 1] >= nums[i + 1]) {
+        if (undefined === nums[i + 2]) {
+          continue;
+        }
+
+        if (nums[i] >= nums[i + 2]) {
+          return false;
+        } else {
+          i++; // Skip the next one.
+        }
+      }
     }
   }
 
