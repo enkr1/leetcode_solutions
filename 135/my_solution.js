2bd234890d99c59ac9d732e5a6759070ddef686d
Jing Hui P <enkr99@gmail.com>
2023-06-14 04:29:01 +0800

    :sparkles: [135] left & right arrays

--- 135/my_solution.js
+++ 135/my_solution.js
@@ -3,55 +3,83 @@
  * @return {number}
  */
 const candy = (ratings) => {
-  let total = 0, prevSum = 1, min = 0, gap = 0;;
-  // let total = 0, groupList = [], tmpGroupCount = 0;
+  let leftList = Array(ratings.length).fill(1),
+    rightList = Array(ratings.length).fill(1);
+
   for (let i = 0; i < ratings.length; i++) {
-    let curr = ratings[i],
-      prev = (undefined === ratings[i - 1]) ? curr : ratings[i - 1],
-      next = (undefined === ratings[i + 1]) ? curr : ratings[i + 1];
-    // console.log("__")
-    // console.log(prev)
-    // console.log(next)
-    // let sum = ((curr > prev) || (curr > next)) ? prevSum + 1 : 1;
-    // if ()
-    let sum = ((curr > prev) || (curr > next)) ? prevSum + 1 : 1;
-    console.log("________________")
-    console.log(`curr:${curr}, sum:${sum}, total:${total}`)
-    if ((prev < curr || prev === curr) && curr > next) { // first time
-      min = (prev === curr) ? sum - 1 : sum;
-      sum = 0;
-      gap++;
-      console.log(`Setting the min for the first time min:${min}, gap:${gap}`)
-    } else if (curr > next) {
-      sum = 0;
-      gap++;
-      console.log(`going down - min:${min}, gap:${gap}`)
-    } else if (curr < next || prev < curr || next === curr) {
-      console.log(`going up / stagnent - min:${min}, gap:${gap}, sum:${sum}`)
-
-      if (min !== 0) {
-        sum = 0;
-        for (let i = 0; i < gap; i++) {
-          sum += sum + 1;
-          console.log(`>>> sum:${sum}`)
-        }
-        console.log(`> sum:${sum}`)
-
-        sum = ((((gap + 1) > min) ? (gap + 1) : min) + sum);
-        console.log(`>> sum:${sum}`)
-        min = 1;
-        gap = 1;
-        total += sum;
-        sum = 0;
-      }
+    let prev = ratings[i - 1];
+    if (prev === undefined) continue;
+    if (prev < ratings[i]) {
+      leftList[i] += leftList[i - 1];
     }
-
-    total += sum; // Going down - dont add
-
-    prevSum = sum;
   }
-  return total;
+
+  for (let i = ratings.length - 1; i >= 0; i--) {
+    let behind = ratings[i + 1];
+    if (behind === undefined) continue;
+    if (behind < ratings[i]) {
+      rightList[i] += rightList[i + 1];
+    }
+  }
+
+  let result = 0;
+  for (let i = 0; i < ratings.length; i++) {
+    result += Math.max(leftList[i], rightList[i]);
+  }
+
+  return result;
 };
+
+// const candy = (ratings) => {
+//   let total = 0, prevSum = 1, min = 0, gap = 0;;
+//   // let total = 0, groupList = [], tmpGroupCount = 0;
+//   for (let i = 0; i < ratings.length; i++) {
+//     let curr = ratings[i],
+//       prev = (undefined === ratings[i - 1]) ? curr : ratings[i - 1],
+//       next = (undefined === ratings[i + 1]) ? curr : ratings[i + 1];
+//     // console.log("__")
+//     // console.log(prev)
+//     // console.log(next)
+//     // let sum = ((curr > prev) || (curr > next)) ? prevSum + 1 : 1;
+//     // if ()
+//     let sum = ((curr > prev) || (curr > next)) ? prevSum + 1 : 1;
+//     console.log("________________")
+//     console.log(`curr:${curr}, sum:${sum}, total:${total}`)
+//     if ((prev < curr || prev === curr) && curr > next) { // first time
+//       min = (prev === curr) ? sum - 1 : sum;
+//       sum = 0;
+//       gap++;
+//       console.log(`Setting the min for the first time min:${min}, gap:${gap}`)
+//     } else if (curr > next) {
+//       sum = 0;
+//       gap++;
+//       console.log(`going down - min:${min}, gap:${gap}`)
+//     } else if (curr < next || prev < curr || next === curr) {
+//       console.log(`going up / stagnent - min:${min}, gap:${gap}, sum:${sum}`)
+//
+//       if (min !== 0) {
+//         sum = 0;
+//         for (let i = 0; i < gap; i++) {
+//           sum += sum + 1;
+//           console.log(`>>> sum:${sum}`)
+//         }
+//         console.log(`> sum:${sum}`)
+//
+//         sum = ((((gap + 1) > min) ? (gap + 1) : min) + sum);
+//         console.log(`>> sum:${sum}`)
+//         min = 1;
+//         gap = 1;
+//         total += sum;
+//         sum = 0;
+//       }
+//     }
+//
+//     total += sum; // Going down - dont add
+//
+//     prevSum = sum;
+//   }
+//   return total;
+// };
 
 let x = candy([1, 0, 2]); // 5
 // let x = candy([1, 2, 2]); // 4
--- 135/solution.js
+++ 135/solution.js
@@ -3,15 +3,29 @@
  * @return {number}
  */
 const candy = (ratings) => {
-  let total = 0, prevSum = 1;
-  for (let i = 0; i < ratings.length; i++) {
-    let curr = ratings[i],
-      prev = (undefined === ratings[i - 1]) ? curr : ratings[i - 1],
-      next = (undefined === ratings[i + 1]) ? curr : ratings[i + 1];
-    let sum = ((curr > prev) || (curr > next)) ? prevSum + 1 : 1;
-    total += sum;
-    prevSum = sum;
-  }
-  return total;
-};
+  let leftList = Array(ratings.length).fill(1),
+    rightList = Array(ratings.length).fill(1);
 
+  for (let i = 0; i < ratings.length; i++) {
+    let prev = ratings[i - 1];
+    if (prev === undefined) continue;
+    if (prev < ratings[i]) {
+      leftList[i] += leftList[i - 1];
+    }
+  }
+
+  for (let i = ratings.length - 1; i >= 0; i--) {
+    let behind = ratings[i + 1];
+    if (behind === undefined) continue;
+    if (behind < ratings[i]) {
+      rightList[i] += rightList[i + 1];
+    }
+  }
+
+  let result = 0;
+  for (let i = 0; i < ratings.length; i++) {
+    result += Math.max(leftList[i], rightList[i]);
+  }
+
+  return result;
+};
