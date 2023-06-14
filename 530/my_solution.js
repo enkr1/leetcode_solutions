6d1e37ab79f7e24f0b79d55be101e35b997b77d8
Jing Hui P <enkr99@gmail.com>
2023-06-15 04:02:03 +0800

    :skull: [530] a1 - Not good

--- 530/my_solution.js
+++ 530/my_solution.js
@@ -10,13 +10,13 @@ class TreeNode {
  * @return {number}
  */
 const getMinimumDifference = (root) => {
-  console.log(Infinity - 1)
-  console.log(1 - Infinity)
-  let queue = [root], min = Infinity;
+  // console.log(Infinity - 1)
+  // console.log(1 - Infinity)
+  let queue = [root], min = Infinity, rootVal = root.val;
 
   while (queue.length > 0) {
-    console.log("queue 1");
-    console.log(queue);
+    // console.log("queue 1");
+    // console.log(queue);
 
     let curr = queue.shift();
     console.log("Processing");
@@ -31,19 +31,36 @@ const getMinimumDifference = (root) => {
       r = curr.right.val;
       queue.push(curr.right);
     }
-    console.log("queue 2");
-    console.log(queue);
+    // console.log("queue 2");
+    // console.log(queue);
 
-    console.log(`min:${min}, q-l:${curr.val - l}, r-q:${r - curr.val}`)
+    console.log(`>>>> min:${min}, l:${l}, r:${r}, Q-l:${curr.val - l}, r-Q:${r - curr.val}, R-l:${rootVal - l}, r-R:${r - rootVal}`)
 
-    min = Math.min(min, curr.val - l, r - curr.val);
+    min = Math.min(min, curr.val - l, r - curr.val, Math.abs(rootVal - l), Math.abs(r - rootVal));
+    console.log(">>>>>> Min:" + min)
   }
 
   return min;
 };
 
 // [4,2,6,1,3]
-let x = getMinimumDifference(new TreeNode(4,
-  new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(6, new TreeNode(5))))
+// let x = getMinimumDifference(new TreeNode(4,
+//   new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(6, new TreeNode(5))))
+
+// [236,104,701,null,227,null,911]
+// let x = getMinimumDifference(
+//   new TreeNode(236,
+//     new TreeNode(104, null, new TreeNode(227)),
+//     new TreeNode(701, null, new TreeNode(911))))
+
+// [1,0,48,null,null,12,49]
+let x = getMinimumDifference(
+  new TreeNode(1,
+    new TreeNode(0),
+    new TreeNode(48,
+      new TreeNode(12), new TreeNode(49))))
+
+
+
 console.log("x")
 console.log(x)
