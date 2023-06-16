1e60471c50db080f5125f6f85b71d66a385ccb3e
Jing Hui P <enkr99@gmail.com>
2023-06-16 23:16:12 +0800

    :skull: [530] in order AJDDAJLLSJKSD

--- 530/solution.js
+++ 530/solution.js
@@ -10,14 +10,50 @@ class TreeNode {
  * @return {number}
  */
 const getMinimumDifference = (root) => {
-  let queue = [root], list = [];
-  while (queue.length > 0) {
-    let curr = queue.shift(), left = curr.left, right = curr.right;
-    list.push(curr.val);
-    if (null !== left) queue.push(left);
-    if (null !== right) queue.push(right);
+  let queue = [root], list = [], visitedList = [];
+  // while (queue.length > 0) {
+  //   let curr = queue.shift(), left = curr.left, right = curr.right;
+  //   list.push(curr.val);
+  //   if (null !== left) queue.push(left);
+  //   if (null !== right) queue.push(right);
+  // }
+
+  const inOrder = (node) => {
+    if (null === node) {
+      visitedList.pop(); // current
+      let parent = visitedList.pop();
+      list.push(parent.val);
+      parent.left = null;
+      return inOrder(parent);
+    } else if (null === node.left) {
+      visitedList.push(node);
+      list.push(node.val);
+      return inOrder(node.right);
+    } else {
+      visitedList.push(node);
+      return inOrder(node.left);
+    }
+    // else {
+    //   // else if (null === node.right) {
+    //   //   list.push(node.val);
+    //   //   let parent = visitedList.pop();
+    //   //   parent.left == null;
+    //   //   return inOrder(parent);
+    //   // } else if (null !== node.left) {
+    //   //   visitedList.push(node);
+    //   //   return inOrder(node.left);
+    //   // } else if (null !== node.right) {
+    //   //   visitedList.push(node);
+    //   //   return inOrder(node.right);
+    //   // }
+    // }
+    // return inOrder();
   }
 
+  inOrder(root);
+
+  console.log(visitedList);
+  console.log(list);
   // let min = Infinity, tmpMax = 0, tmpMin = Infinity;
   // for (let i = 0; i < list.length; i++) {
   //   tmpMax = Math.max(tmpMax, list[i]);
