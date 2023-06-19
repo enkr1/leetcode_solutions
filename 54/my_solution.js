2b57a4dfb49bd4375743c1b0c1371398be2d28b6
Jing Hui P <64194467+enkr1@users.noreply.github.com>
2023-06-19 22:43:23 +0800

    :cry: [54] wtf

--- 54/my_solution.js
+++ 54/my_solution.js
@@ -3,102 +3,38 @@
  * @return {number[]}
  */
 const spiralOrder = (matrix) => {
-  if (matrix.length === 1) return matrix[0];
-  let nextDirection = new Map(), formula = new Map();
-  nextDirection.set("r", "b")
-  nextDirection.set("b", "l")
-  nextDirection.set("l", "t")
-  nextDirection.set("t", "r")
+  let left = 0, right = matrix[0].length, top = 0, bot = matrix.length, spiralList = [];
 
-  formula.set("r", { r: 0, c: 1 })
-  formula.set("b", { r: 1, c: 0 })
-  formula.set("l", { r: 0, c: -1 })
-  formula.set("t", { r: -1, c: 0 })
+  while (left < right && top < bot) {
+    for (let i = left; i < right; i++) {
+      spiralList.push(matrix[top][i]);
+    }
+    top += 1;
 
-  console.log(nextDirection)
-  console.log(formula)
-
-  let spiralList = [];
-
-  const moveNext = (r, c, dir) => {
-    let nextDir = nextDirection.get(dir);
-    let { r: newR, c: newC } = formula.get(nextDir);
-    console.log(`>> Going nextDir:${nextDir}, newR:${r + newR + (r === matrix.length ? -1 : 0)}, newC:${c + newC + (c === matrix[0].length ? -1 : 0)}`)
-    // let isREdge = r > matrix.length, isCEdge = c > matrix[0].length;
-    // console.log("matrix[0].length ")
-    // console.log(matrix[0].length)
-
-    newR += r + (r === matrix.length ? -1 : 0) + (r < 0 ? 1 : 0)
-    newC += c + (c === matrix[0].length ? -1 : 0) + (c < 0 ? 1 : 0)
-
-    return { r: newR, c: newC, dir: nextDir };
-  }
-
-  const explore = (r, c, dir, tBorder, rBorder, bBorder, lBorder) => {
-    console.log(`> r:${r}, c:${c}, dir:${dir}, tBorder:${tBorder}, rBorder:${rBorder}, bBorder:${bBorder}, lBorder:${lBorder}`)
-
-    // edge
-    console.log(r - bBorder === matrix.length)
-    console.log(c - rBorder === matrix[0].length)
-    console.log((dir === "t" && r < 0 + tBorder))
-    console.log((dir === "l" && c < 0 + lBorder))
-    console.log((dir === "r" && c + rBorder === matrix[0].length))
-    console.log((dir === "b" && r + bBorder === matrix.length))
-    if (
-      r - bBorder === matrix.length ||
-      c - rBorder === matrix[0].length ||
-      (dir === "t" && r < 0 + tBorder) ||
-      (dir === "l" && c < 0 + lBorder) ||
-      (dir === "r" && c + rBorder === matrix[0].length) ||
-      (dir === "b" && r + bBorder === matrix.length)
-    ) {
-      let next = moveNext(r, c, dir);
-
-      explore(
-        next.r + (next.dir === "r" ? tBorder : 0) + (next.dir === "l" ? -bBorder : 0),
-        next.c + (next.dir === "b" ? -rBorder : 0) + (next.dir === "t" ? lBorder : 0),
-        next.dir,
-        tBorder + (next.dir === "r" ? 1 : 0),
-        rBorder + (next.dir === "b" ? 1 : 0),
-        bBorder + (next.dir === "l" ? 1 : 0),
-        lBorder + (next.dir === "t" ? 1 : 0));
-      return;
+    if (left >= right || top >= bot) {
+      console.log("??")
+      break;
     }
 
-    // visited - end
-    // if (matrix.length < tries) {
-    //   return;
-    // }
-
-    if (Infinity === matrix[r][c]) {
-      // let next = moveNext(r, c, dir);
-      // explore(next.r, next.c, next.dir, tries + 1);
-
-      //       let nextDir = nextDirection.get(dir);
-      //       let { r: newR, c: newC } = formula.get(nextDir);
-      //       console.log(`>> Going newR:${r + newR + (r === matrix.length ? -1 : 0)}, newC:${c + newC + (c === matrix[0].length ? -1 : 0)}`)
-      //       // let isREdge = r > matrix.length, isCEdge = c > matrix[0].length;
-      //       console.log("matrix[0].length ")
-      //       console.log(matrix[0].length)
-      //
-      //       newR += r + (r === matrix.length ? -1 : 0) + (r < 0 ? 1 : 0) - ((formula.get(dir).r));
-      //       newC += c + (c === matrix[0].length ? -1 : 0) + (c < 0 ? 1 : 0) - ((formula.get(dir).c));
-      //
-      //       explore(newR, newC, nextDir, tBorder, rBorder, bBorder, lBorder);
-      return;
+    for (let i = top; i < bot; i++) {
+      spiralList.push(matrix[i][right - 1]);
     }
+    right -= 1;
 
-    spiralList.push(matrix[r][c]);
-    matrix[r][c] = Infinity;
 
-    let dirFormula = formula.get(dir);
-    // console.log(`> r:${r}, c:${c}, addR:${dirFormula.r}, addC:${dirFormula.c}`)
-    // if next we turn
-    // matrix[r + dirFormula.r][c + dirFormula.c];
-    explore(r + dirFormula.r, c + dirFormula.c, dir, tBorder, rBorder, bBorder, lBorder)
+    for (let i = right - 1; i >= left; i--) {
+      spiralList.push(matrix[bot - 1][i]);
+    }
+    bot -= 1;
+
+    for (let i = bot - 1; i >= top; i--) {
+      spiralList.push(matrix[i][left]);
+    }
+    left += 1;
+
+    console.log(`> l:${left}, r:${right}, t:${top}, b:${bot}`)
   }
 
-  explore(0, 0, "r", 1, 0, 0, 0);
   console.log(matrix)
   return spiralList;
 };
@@ -120,13 +56,13 @@ let x = null;
 // ]);
 
 // [1,2,3,4,5,10,15,20,25,24,23,22,21,16,11,6,7,8,9,14,19,18,17,12,13]
-x = spiralOrder([
-  [1, 2, 3, 4, 5],
-  [6, 7, 8, 9, 10],
-  [11, 12, 13, 14, 15],
-  [16, 17, 18, 19, 20],
-  [21, 22, 23, 24, 25]
-]);
+// x = spiralOrder([
+//   [1, 2, 3, 4, 5],
+//   [6, 7, 8, 9, 10],
+//   [11, 12, 13, 14, 15],
+//   [16, 17, 18, 19, 20],
+//   [21, 22, 23, 24, 25]
+// ]);
 
 // x = spiralOrder([[1]]);
 
@@ -139,10 +75,21 @@ x = spiralOrder([
 // ]);
 
 // [1,2,3,4,8,12,11,10,9,5,6,7]
+x = spiralOrder([
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12]
+]);
+
 // x = spiralOrder([
-//   [1, 2, 3, 4],
-//   [5, 6, 7, 8],
-//   [9, 10, 11, 12]
+//   [1, 2, 3, 4]
+// ]);
+
+// x = spiralOrder([
+//   [1],
+//   [2],
+//   [3],
+//   [4]
 // ]);
 
 // [1,2,3,6,9,8,7,4,5]
