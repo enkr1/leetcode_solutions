0bf3e604a7651398fee5cd889e0c82a1b527a4f2
Jing Hui P <64194467+enkr1@users.noreply.github.com>
2023-06-21 20:45:51 +0800

    :sparkles: [832] Flipping an Image - inplace

--- 832/my_solution.js
+++ 832/my_solution.js
@@ -3,15 +3,21 @@
  * @return {number[][]}
  */
 const flipAndInvertImage = (image) => {
-
-  let result = Array.from({ length: image.length }, () => Array.from({ length: image[0].length }, () => []));
-  console.log(result)
+  // let result = Array.from({ length: image.length }, () => Array.from({ length: image[0].length }, () => []));
+  // console.log(result)
   for (let row = 0; row < image.length; row++) {
+    let tmpArray = Array.from({ length: image[row].length }, () => []);
+    console.log(tmpArray)
     for (let col = 0; col < image[row].length; col++) {
-      result[row][image[row].length - 1 - col] = image[row][col] ^ 1;
+      // result[row][image[row].length - 1 - col] = image[row][col] ^ 1;
+      tmpArray[image[row].length - 1 - col] = image[row][col] ^ 1;
     }
+    console.log(tmpArray)
+    console.log(image)
+    image[row] = tmpArray
+    console.log(image)
   }
-  return result;
+  return image;
 };
 
 let x = null;
