170948b29ece22133202bcb11e77770733f493df
Jing Hui P <enkr99@gmail.com>
2023-06-25 11:47:56 +0800

    :cool: [100] Better recursion

--- 100/solution.js
+++ 100/solution.js
@@ -13,28 +13,34 @@ class TreeNode {
  */
 const isSameTree = (p, q) => {
   if (p?.val !== q?.val) return false;
-  let pQueue = [p], qQueue = [q];
-
-  const extractLeft = (node, list) => {
-    if (null === node) return;
-    list.push(node.left);
-    return node.left?.val;
-  }
-
-  const extractRight = (node, list) => {
-    if (null == node) return;
-    list.push(node.right);
-    return node.right?.val;
-  }
-
-  while (0 < pQueue.length && 0 < qQueue.length) {
-    let currP = pQueue.shift(), currQ = qQueue.shift();
-    if (currP?.val !== currQ?.val) return false;
-    extractLeft(currP, pQueue)
-    extractLeft(currQ, qQueue)
-    extractRight(currP, pQueue)
-    extractRight(currQ, qQueue)
-  }
-
-  return true;
+  if (null === p && null === q) return true;
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
+
+// const isSameTree = (p, q) => {
+//   if (p?.val !== q?.val) return false;
+//   let pQueue = [p], qQueue = [q];
+//
+//   const extractLeft = (node, list) => {
+//     if (null === node) return;
+//     list.push(node.left);
+//     return node.left?.val;
+//   }
+//
+//   const extractRight = (node, list) => {
+//     if (null == node) return;
+//     list.push(node.right);
+//     return node.right?.val;
+//   }
+//
+//   while (0 < pQueue.length && 0 < qQueue.length) {
+//     let currP = pQueue.shift(), currQ = qQueue.shift();
+//     if (currP?.val !== currQ?.val) return false;
+//     extractLeft(currP, pQueue)
+//     extractLeft(currQ, qQueue)
+//     extractRight(currP, pQueue)
+//     extractRight(currQ, qQueue)
+//   }
+//
+//   return true;
+// };
