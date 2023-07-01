81c91220574f7fe2917537023b0c109fb63be96e
Jing Hui P <enkr99@gmail.com>
2023-07-02 04:45:31 +0800

    :white_check_mark: [1768]

--- 1768/my_solution.js
+++ 1768/my_solution.js
@@ -4,27 +4,52 @@
  * @return {string}
  */
 var mergeAlternately = function (word1, word2) {
-  let str = "";
+  let str = "", processWord = [], appendWord = []
+
   word1 = word1.split(""), word2 = word2.split("");
-  while (word1.length > 0 || word2.length > 0) {
-    let curr1 = "", curr2 = "";
-    console.log(word1)
-    console.log(word2)
-    if (word1.length > 0) {
-      curr1 = word1.shift();
-    }
+  // use the short one to process
+  // if (word1.length <= word2.length) {
+  processWord = word1;
+  appendWord = word2;
+  // } else {
+  //   processWord = word2;
+  //   appendWord = word1;
+  // }
 
-    if (word2.length > 0) {
-      curr2 = word2.shift();
-    }
-
-    str += curr1 + curr2;
+  for (let i = 0; i < word1.length; i++) {
+    console.log("word1")
+    console.log(word1[i])
+    str += word1[i];
+    if (word2.length > 0) str += word2.shift();
   }
+
+  console.log("str")
+  console.log(str)
+
+  str += word2.join("");
+
+  //   while (word1.length > 0 || word2.length > 0) {
+  //     let curr1 = "", curr2 = "";
+  //     console.log(word1)
+  //     console.log(word2)
+  //     if (word1.length > 0) {
+  //       curr1 = word1.shift();
+  //     }
+  //
+  //     if (word2.length > 0) {
+  //       curr2 = word2.shift();
+  //     }
+  //
+  //     str += curr1 + curr2;
+  //   }
 
   return str;
 };
+
 let x =
-  mergeAlternately("abc", "pqr")
+  // mergeAlternately("abc", "pqr")
+  mergeAlternately("ab", "pqrs")
+  // mergeAlternately("abcd", "pq")
 
 console.log("Res")
 console.log(x)
--- 1768/solution.js
+++ 1768/solution.js
@@ -5,21 +5,35 @@
  */
 var mergeAlternately = function (word1, word2) {
   let str = "";
+
   word1 = word1.split(""), word2 = word2.split("");
-  while (word1.length > 0 || word2.length > 0) {
-    let curr1 = "", curr2 = "";
-    console.log(word1)
-    console.log(word2)
-    if (word1.length > 0) {
-      curr1 = word1.shift();
-    }
-
-    if (word2.length > 0) {
-      curr2 = word2.shift();
-    }
-
-    str += curr1 + curr2;
+  for (let i = 0; i < word1.length; i++) {
+    str += word1[i];
+    if (word2.length > 0) str += word2.shift();
   }
+
+  str += word2.join("");
 
   return str;
 };
+
+// var mergeAlternately = function (word1, word2) {
+//   let str = "";
+//   word1 = word1.split(""), word2 = word2.split("");
+//   while (word1.length > 0 || word2.length > 0) {
+//     let curr1 = "", curr2 = "";
+//     console.log(word1)
+//     console.log(word2)
+//     if (word1.length > 0) {
+//       curr1 = word1.shift();
+//     }
+//
+//     if (word2.length > 0) {
+//       curr2 = word2.shift();
+//     }
+//
+//     str += curr1 + curr2;
+//   }
+//
+//   return str;
+// };
