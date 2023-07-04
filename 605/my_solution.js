39ffb0b0fd9a5b6e77ff3922749769da67f2f2b6
Jing Hui P <enkr99@gmail.com>
2023-07-05 03:03:19 +0800

    :white_check_mark: [605]

--- 605/my_solution.js
+++ 605/my_solution.js
@@ -4,12 +4,35 @@
  * @return {boolean}
  */
 var canPlaceFlowers = function (flowerbed, n) {
-  if (n === 0) return true;
-
-  let placeNeeded = 1;
-  for (let i = 0; i < n; i++) {
-    placeNeeded += 2;
+  for (let i = 0; i < flowerbed.length; i++) {
+    console.log(`i:${i}`)
+    if (
+      ((flowerbed[i - 1] ?? 0) === 0) &&
+      ((flowerbed[i + 1] ?? 0) === 0) &&
+      (flowerbed[i] === 0)
+    ) {
+      console.log(`[i - 1]: ${(flowerbed[i - 1])}, [i]: ${(flowerbed[i])}, [i + 1]: ${(flowerbed[i + 1])}`)
+      console.log(((flowerbed[i - 1] || 0) === 0))
+      console.log(((flowerbed[i + 1] || 0) === 0))
+      console.log((flowerbed[i] === 0))
+      console.log("what")
+      flowerbed[i] = 1;
+      n--;
+    }
   }
 
-  return (flowerbed.length - 2) >= placeNeeded;
+  console.log(`-> n:${n}`)
+
+  return n <= 0;
 };
+
+let x =
+  //   canPlaceFlowers([1, 0, 0, 0, 1], 1) // true
+  canPlaceFlowers([1, 0, 0, 0, 0, 1], 2) // false
+  // canPlaceFlowers([0, 0, 1, 0, 1], 1) // true
+  // canPlaceFlowers([1, 0, 0, 0, 1, 0, 0], 2) // true
+  // canPlaceFlowers([0, 0, 1, 0, 0], 1) // true
+  // canPlaceFlowers([0, 1, 0, 1, 0, 1, 0, 0], 1) // true
+
+console.log("Result")
+console.log(x)
