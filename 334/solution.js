01dbd91e29c769f068104819cbaedf2cc476fda4
Jing Hui P <enkr99@gmail.com>
2023-07-06 03:14:31 +0800

    :sparkles: [334] O(1) solution in space

--- 334/solution.js
+++ 334/solution.js
@@ -3,18 +3,34 @@
  * @return {boolean}
  */
 var increasingTriplet = function (nums) {
-  let minToRight = [], maxToLeft = [];
-  for (let i = 0; i < nums.length; i++) {
-    minToRight[i] = Math.min((minToRight[i - 1] ?? Infinity), nums[i])
-  }
-
-  for (let i = nums.length - 1; i >= 0; i--) {
-    maxToLeft[i] = Math.max((maxToLeft[i + 1] ?? -Infinity), nums[i])
-  }
+  let first = Infinity, second = Infinity;
 
   for (let i = 0; i < nums.length; i++) {
-    if (minToRight[i] < nums[i] && nums[i] < maxToLeft[i]) return true;
+    if (nums[i] <= first) {
+      first = nums[i];
+    } else if (nums[i] <= second) {
+      second = nums[i];
+    } else {
+      return true;
+    }
   }
 
   return false;
 };
+
+// var increasingTriplet = function (nums) {
+//   let minToRight = [], maxToLeft = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     minToRight[i] = Math.min((minToRight[i - 1] ?? Infinity), nums[i])
+//   }
+//
+//   for (let i = nums.length - 1; i >= 0; i--) {
+//     maxToLeft[i] = Math.max((maxToLeft[i + 1] ?? -Infinity), nums[i])
+//   }
+//
+//   for (let i = 0; i < nums.length; i++) {
+//     if (minToRight[i] < nums[i] && nums[i] < maxToLeft[i]) return true;
+//   }
+//
+//   return false;
+// };
