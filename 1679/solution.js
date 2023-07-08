b9c1e04cb59add64c69a3c6fa4ef162d0b2118ed
Jing Hui P <enkr99@gmail.com>
2023-07-08 18:14:12 +0800

    :white_check_mark: [1679] n log n

--- 1679/solution.js
+++ 1679/solution.js
@@ -4,21 +4,20 @@
  * @return {number}
  */
 var maxOperations = function (nums, k) {
-  let complimentMap = new Map(), operationCount = 0
+  let l = 0, r = nums.length - 1, operationCount = 0
 
-  for (let i = 0; i < nums.length; i++) {
-    let compliment = k - nums[i]
-    complimentMap.set(compliment, (complimentMap.get(compliment) ?? 0) + 1)
-  }
-
-  for (let i = 0; i < nums.length; i++) {
-    if (complimentMap.has(nums[i]) && complimentMap.get(nums[i]) > 0) {
-      complimentMap.set(nums[i], complimentMap.get(nums[i]) - 1);
-      complimentMap.set(k - nums[i], complimentMap.get(k - nums[i]) - 1);
+  nums = nums.sort((a, b) => a - b);
+  while (l < r) {
+    if ((nums[l] + nums[r]) === k) {
       operationCount++;
+      r--;
+      l++;
+    } else if ((nums[l] + nums[r]) > k) {
+      r--;
+    } else {
+      l++;
     }
   }
 
   return operationCount;
-
 };
