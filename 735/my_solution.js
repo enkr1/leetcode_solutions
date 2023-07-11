2761a73481abfa458ff97ff863a1b89e9c47a02a
Jing Hui P <enkr99@gmail.com>
2023-07-12 03:30:50 +0800

    :construction: [735] recursion

--- 735/my_solution.js
+++ 735/my_solution.js
@@ -5,6 +5,29 @@
 var asteroidCollision = function (asteroids) {
   let result = []
 
+  //   const pushOrIgnore = (n) => {
+  //     let last = result[result.length - 1]
+  //     console.log(`pushOrIgnore - n: ${n}, l: ${last}`)
+  //     console.log(result)
+  //
+  //
+  //     if (last > n || last < 0) {
+  //       // if ((result[result.length - 1] ?? -Infinity) < n) result.push(-n)
+  //       return
+  //     }
+  //
+  //     if (last == n) {
+  //       console.log("l===n")
+  //       result.pop()
+  //     } else if (last < n) {
+  //       console.log("l<n")
+  //       result.pop()
+  //       result.push(-n)
+  //     }
+  //
+  //     return pushOrIgnore(n)
+  //   }
+
   for (let i = 0; i < asteroids.length; i++) {
     let curr = asteroids[i]
     if (curr > 0 || i === 0) {
@@ -16,15 +39,19 @@ var asteroidCollision = function (asteroids) {
     console.log(`> i: ${i}, curr: ${curr}`)
     console.log(result)
     curr *= -1;
+
     // let last = result[result.length - 1];
     // console.log(`> last: ${last}, curr: ${curr}`)
 
-    let idx = result.length - 1, collided = false;
+    let last = null, idx = result.length - 1, collided = false;
     while (idx >= 0) {
-      let last = result[idx] // last in result
+      last = result[idx] // last in result
       console.log(`-> idx: ${idx}, last: ${last}`)
 
-      if ((last < 0) || (last > curr)) break // prev/last > curr
+      if (
+        (last < 0) // same direction
+        || (last > curr) // bigger
+      ) break // prev/last > curr
       // all positive
 
       console.log(result)
@@ -45,20 +72,23 @@ var asteroidCollision = function (asteroids) {
     console.log(result)
     // console.log(`--> last: ${last}, curr: ${curr}`)
     // if (last < 0) last *= -1;
-    if ((result[result.length - 1] ?? -Infinity) < curr) result.push(-curr)
+    // if ((result[result.length - 1] ?? -Infinity) < curr) result.push(-curr)
+    if (last < curr) result.push(-curr)
     // curr is <
+
+    // pushOrIgnore(curr)
   }
 
   return result;
 };
 
 let x =
-  asteroidCollision([-2, 1, -2, -2]) // [-2,-2,-2]
-// asteroidCollision([-2, -2, 1, -1]) // [-2,-2]
-// asteroidCollision([-2, -1, 1, 2])
-// asteroidCollision([5, 10, -15])
-// asteroidCollision([5, 10, -5])
-// asteroidCollision([8, -8])
-// asteroidCollision([10, 2, -5])
+  // asteroidCollision([-2, 1, -2, -2]) // [-2,-2,-2]
+  // asteroidCollision([-2, -2, 1, -1]) // [-2,-2]
+  // asteroidCollision([-2, -1, 1, 2])
+  // asteroidCollision([5, 10, -15])
+  // asteroidCollision([5, 10, -5])
+  // asteroidCollision([8, -8])
+  asteroidCollision([10, 2, -5])
 console.log("Res")
 console.log(x)
